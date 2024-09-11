2a9bdb89b25cd914061e5199d6996a2ec213f2d1
frogcovenant <judithvelram@gmail.com>
2024-09-10 21:38:13 -0600

    chore/cleanup-project-and-dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button"
-
 export default function App() {
-  return (
-    <div>
-      <Button>Click me</Button>
-    </div>
-  )
+	return (
+	<div>
+		Nothing here
+	</div>
+	)
 }
