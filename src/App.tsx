af7524263e7d4777412b26781190745982888b0e
frogcovenant <judithvelram@gmail.com>
2024-09-16 20:08:26 -0600

    feat/update-team-lists-on-team-selection

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,38 @@ export default function App() {
 		setTeamOptions(teams.map((team) => team.teamName));
 	}, []); // Empty dependency array means this runs only once
 
+	function handleSelectTeam(teamIndex: number): void {
+		const selectedTeam = teamOptions[teamIndex];
+
+		// TODO: for now we always add to the list with the least teams.
+		// We must KEEP BALANCE across SCHOOLS instead.
+		const teamListInfoSortedByAvailability = [...teamListInfo];
+		teamListInfoSortedByAvailability.sort((teamListA, teamListB) => {
+			if (teamListA.teams.length < teamListB.teams.length) {
+				return -1;
+			}
+			if (teamListA.teams.length > teamListB.teams.length) {
+				return 1;
+			}
+			return 0;
+		});
+		teamListInfoSortedByAvailability[0].teams.push(selectedTeam);
+
+		// remove selected team from the options list
+		setTeamOptions(teamOptions.filter((team) => {
+			return team !== selectedTeam;
+		}));
+    }
+
 	return (
 		<div className='main-page'>
-			<TeamSelector teamOptions={teamOptions}/>
-			<Teams teamListInfo={teamListInfo}/>
+			<TeamSelector 
+				teamOptions={teamOptions}
+				onSelectTeam={handleSelectTeam}
+			/>
+			<Teams
+				teamListInfo={teamListInfo}
+			/>
 		</div>
 	);
 }
--- src/components/TeamSelector.tsx
+++ src/components/TeamSelector.tsx
@@ -1,14 +1,32 @@
+import React, { useState } from 'react';
+
 import './TeamSelector.css';
 
 interface TeamSelectorProps {
-	teamOptions: string[];
+    teamOptions: string[];
+    onSelectTeam: (teamIndex: number) => void;
 }
 
 export default function TeamSelector(props: TeamSelectorProps) {
+    const [selectedValue, setSelectedValue] = useState<number | ''>('');
+
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedValue(Number(event.target.value));
+    };
+
+    const handleButtonClick = () => {
+        // TODO: currently the state of 'selectedValue' is not cleared properly when adding a team that is at the bottom
+        // of the select tag. The state should reset to default value instead of having extra checks here.
+        // This will also fix the issue of the selector not returning to the placeholder after adding a team.
+        if (selectedValue !== '' && props.teamOptions.length !== 0 && selectedValue < props.teamOptions.length) {
+            props.onSelectTeam(selectedValue);
+        }
+    };
+
     return (
         <div className='team-selector'>
             <h6>Nombre del equipo</h6>
-            <select>
+            <select value={selectedValue} onChange={handleSelectChange}>
                 <option value="">Selecciona un equipo</option>
                 {props.teamOptions.map((value, index)=>{
                     return (
@@ -16,7 +34,7 @@ export default function TeamSelector(props: TeamSelectorProps) {
                     )
                 })}
             </select>
-            <button>+</button>
+            <button onClick={handleButtonClick}>+</button>
         </div>
     );
 }
