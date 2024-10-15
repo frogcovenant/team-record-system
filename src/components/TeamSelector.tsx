4034311c8516b45cebde0c842bf38cd38c11f2ca
frogcovenant <judithvelram@gmail.com>
2024-10-14 18:50:11 -0600

    feat/allow-type-and-filter-on-team-input

--- src/components/TeamSelector.tsx
+++ src/components/TeamSelector.tsx
@@ -8,32 +8,42 @@ interface TeamSelectorProps {
 }
 
 export default function TeamSelector(props: TeamSelectorProps) {
-    const [selectedValue, setSelectedValue] = useState<number | ''>('');
+    const [selectedValue, setSelectedValue] = useState<string>('');
 
-    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedValue(Number(event.target.value));
+    const handleSelectChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedValue(event.target.value);
     };
 
     const handleButtonClick = () => {
-        // TODO: currently the state of 'selectedValue' is not cleared properly when adding a team that is at the bottom
-        // of the select tag. The state should reset to default value instead of having extra checks here.
-        // This will also fix the issue of the selector not returning to the placeholder after adding a team.
-        if (selectedValue !== '' && props.teamOptions.length !== 0 && selectedValue < props.teamOptions.length) {
-            props.onSelectTeam(selectedValue);
+        // Find the index of the selected team in the teamOptions array
+        const teamIndex = props.teamOptions.indexOf(selectedValue);
+
+        if (teamIndex !== -1) {
+            props.onSelectTeam(teamIndex);
+            // Clear the input field after the team is selected
+            setSelectedValue('');
+        } else {
+            console.log("Team not found or invalid input");
         }
     };
 
     return (
         <div className='team-selector'>
             <h6>Nombre del equipo</h6>
-            <select value={selectedValue} onChange={handleSelectChange}>
-                <option value="">Selecciona un equipo</option>
-                {props.teamOptions.map((value, index)=>{
+            <input 
+                type="text"
+                list="team-options"
+                value={selectedValue}
+                onChange={handleSelectChange}
+                placeholder="Selecciona o escribe un equipo"
+            />
+            <datalist id="team-options">
+                {props.teamOptions.map((value, index) => {
                     return (
-                        <option key={index} value={index}>{value}</option>  
+                        <option key={index} value={value}></option>  
                     )
                 })}
-            </select>
+            </datalist>
             <button onClick={handleButtonClick}>+</button>
         </div>
     );
